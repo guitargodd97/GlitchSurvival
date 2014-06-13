b8e7128ed3b4daf864046803a284adc23b181567
guitargodd97 <guitargodd97@gmail.com>
2014-06-12 21:18:15 -0400

    Timer initiated

--- Glitch Survival-core/src/com/heidenreich/glitch/objects/Enemy.java
+++ Glitch Survival-core/src/com/heidenreich/glitch/objects/Enemy.java
@@ -1,6 +1,7 @@
 package com.heidenreich.glitch.objects;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.heidenreich.glitch.GlitchGame;
 import com.heidenreich.glitch.handlers.Animation;
@@ -9,7 +10,10 @@ public class Enemy {
 
 	private Animation animation;
 	private boolean ground;
+	private float curDistance;
+	private float distance;
 	private float gravityMultiplier = 0.0175f;
+	private Rectangle rect;
 	private Vector2 location;
 	private Vector2 velocity;
 
@@ -17,19 +21,23 @@ public class Enemy {
 		animation = new Animation(GlitchGame.assets.getAnimatedSprite("enemy",
 				8), 1 / 20f);
 		location = new Vector2(200, 420);
-		velocity = new Vector2(0, 0);
+		velocity = new Vector2(1.9f, 0);
 		ground = false;
+		rect = new Rectangle(location.x + 5, location.y, 20, 20);
+		distance = 180;
 	}
 
 	public void update(float delta) {
 		handleMovement();
-		location.add(velocity);
+		rect.setPosition(location);
+		location.add(new Vector2(0, velocity.y));
 		animation.update(delta);
 	}
 
 	public void render(SpriteBatch batch) {
 		batch.begin();
-		animation.getFrame().setPosition(location.x, location.y);
+		animation.getFrame().setPosition(
+				location.x - animation.getFrame().getWidth() / 2, location.y);
 		animation.getFrame().draw(batch);
 		batch.end();
 	}
@@ -37,6 +45,32 @@ public class Enemy {
 	private void handleMovement() {
 		if (!ground)
 			velocity.y += GlitchGame.GRAVITY * gravityMultiplier;
+		else {
+			location.add(new Vector2(velocity.x, 0));
+			curDistance += Math.abs(velocity.x);
+			if (curDistance >= distance || location.x > 790 || location.x < 10) {
+				curDistance -= distance;
+				velocity.x *= -1;
+			}
+		}
+
+	}
+
+	public void setGround(boolean ground) {
+		this.ground = ground;
+	}
+
+	public Rectangle getRect() {
+		return rect;
+	}
+
+	public void setY(float y) {
+		location.y = y;
+		velocity.y = 0;
+	}
+
+	public boolean checkFloor() {
+		return location.y < GlitchGame.FLOOR;
 	}
 
 }
--- Glitch Survival-core/src/com/heidenreich/glitch/screens/GameScreen.java
+++ Glitch Survival-core/src/com/heidenreich/glitch/screens/GameScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.heidenreich.glitch.GlitchGame;
 import com.heidenreich.glitch.handlers.Background;
 import com.heidenreich.glitch.handlers.GlitchInput;
@@ -16,8 +17,13 @@ import com.heidenreich.glitch.objects.Player;
 public class GameScreen implements Screen {
 
 	private Background background;
+	private boolean running;
 	private Enemy enemy;
 	private GlitchGame game;
+	private int minutes;
+	private int seconds;
+	private long curTime;
+	private long startTime;
 	private Platform platform;
 	private Player player;
 	private Sprite base;
@@ -33,37 +39,69 @@ public class GameScreen implements Screen {
 		player = new Player();
 		enemy = new Enemy();
 		platform = new Platform(new Vector2(220, 120));
+		running = true;
+		startTime = TimeUtils.millis();
+		curTime = TimeUtils.millis();
 	}
 
 	public void render(float delta) {
 		// Clears the screen
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		if (running) {
+			background.update(delta);
+			background.render(batch);
 
-		background.update(delta);
-		background.render(batch);
+			batch.begin();
+			base.draw(batch);
+			batch.end();
 
-		batch.begin();
-		base.draw(batch);
-		batch.end();
+			platform.render(batch);
 
-		platform.render(batch);
+			player.update(delta);
+			player.render(batch);
 
-		player.update(delta);
-		player.render(batch);
+			enemy.update(delta);
+			enemy.render(batch);
 
-		enemy.update(delta);
-		enemy.render(batch);
+			if (!player.isJumping()
+					&& platform.getRect().overlaps(player.getRect())) {
+				player.setGround(true);
+				player.setY(platform.getRect().getY() + 14);
+			} else if (player.checkFloor()) {
+				player.setGround(true);
+				player.setY(GlitchGame.FLOOR);
+			} else {
+				if (GlitchInput.isReleased())
+					player.setGround(false);
+			}
 
-		if (!player.isJumping() && platform.getRect().overlaps(player.getRect())) {
-			player.setGround(true);
-			player.setY(platform.getRect().getY() + 14);
-		} else if (player.checkFloor()) {
-			player.setGround(true);
-			player.setY(GlitchGame.FLOOR);
+			if (platform.getRect().overlaps(enemy.getRect())) {
+				enemy.setGround(true);
+				enemy.setY(platform.getRect().getY() + 14);
+			} else if (enemy.checkFloor()) {
+				enemy.setGround(true);
+				enemy.setY(GlitchGame.FLOOR);
+			} else {
+				enemy.setGround(false);
+			}
+
+			if (player.getRect().overlaps(enemy.getRect()))
+				running = false;
+			curTime = TimeUtils.millis();
+			int x = (int) ((curTime - startTime) / 1000 + 0.5);
+			minutes = (int) (x / 60);
+			seconds = x % 60;
+			String time = "";
+			if (seconds < 10)
+				time = minutes + ":0" + seconds;
+			else
+				time = minutes + ":" + seconds;
+			batch.begin();
+			game.getFont().draw(batch, time, 330, 55);
+			batch.end();
 		} else {
-			if (GlitchInput.isReleased())
-				player.setGround(false);
+
 		}
 	}
 
